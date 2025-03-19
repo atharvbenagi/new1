930384e8ec7edf92185f1f28f0b6a9d5d96914e9
atharvbenagi <103270480+atharvbenagi@users.noreply.github.com>
2025-03-19 19:39:16 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/new1/",  // This is required for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
